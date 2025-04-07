b63e08c13434fc36bb6b43bc4889d90aea51a693
Robinson Garcia <edifactmxrobinsongarcia@gmail.com>
2025-04-07 14:01:16 -0600

    agregando template de bootstrap e index.html

---
+++ src/main/java/mx/com/edifact/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.edifact.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author rgarcia
+ */
+@Controller
+public class HomeController {
+    
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
